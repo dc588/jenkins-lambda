c533f47c110cd716d7076d0744085ddfc0645491
Julie Chacko <juliechacko@Julies-MacBook-Air.local>
2018-03-18 22:28:48 -0500

    kick off jenkins job

--- src/jenkins-lambda.py
+++ src/jenkins-lambda.py
@@ -10,5 +10,6 @@ def lambda_handler(event, context):
     print("value1 = " + event['key1'])
     print("value2 = " + event['key2'])
     print("value3 = " + event['key3'])
+    print("value4 = " + event['key3'])
     return event['key1']  # Echo back the first key value
     #raise Exception('Something went wrong')
